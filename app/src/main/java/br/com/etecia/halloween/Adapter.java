bab4b5cb6dd3593a6d20e192eec2bfa9261e1686
alexnezumi <alexsilva.viana2006@gmail.com>
2024-10-31 21:59:58 -0300

    lista

--- app/src/main/java/br/com/etecia/halloween/Adapter.java
+++ app/src/main/java/br/com/etecia/halloween/Adapter.java
@@ -3,6 +3,8 @@ package br.com.etecia.halloween;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,9 +34,14 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView modeloTitulo;
+
+        ImageView modeloImagem;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            modeloImagem = itemView.findViewById(R.id.modeloimg);
+            modeloTitulo = itemView.findViewById(R.id.idmodelotxt);
         }
     }
 }
